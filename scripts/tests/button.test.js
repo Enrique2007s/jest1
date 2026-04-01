ad01fd1a58974905e9c3b34b6896069dd7319494
Enrique2007s <enriquesmith458@gmail.com>
2026-03-31 20:28:57 -0400

    edited button.test.js for better testing

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,12 +1,19 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe('DOM tests', () => {
-    TestScheduler('expects p content to change on button click', () => {
+    test('expects p content to change on button click', () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toBe("You Clicked");
+    });
+    test('h1 should exist', () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
